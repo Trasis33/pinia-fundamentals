19cd4676e6bff6296c3950fa23a4d5422a2a9b4f
Fredrik Langå <frlaga@electra.se>
2024-05-31 16:38:20 +0200

    add todo app

---
+++ src/stores/todoList.js
@@ -0,0 +1,27 @@
+import { defineStore } from "pinia";
+
+export const useTodoListStore = defineStore("TodoList", {
+  //state
+  state: () => ({
+    todoList: [],
+    id: 0,
+  }),
+  //actions
+  actions: {
+    addTodo(item) {
+      this.todoList.push({ item, id: this.id++, completed: false });
+    },
+    deleteTodo(itemId) {
+      this.todoList = this.todoList.filter((object) => {
+        return object.id !== itemId;
+      });
+    },
+    toggleCompleted(idToFind) {
+      const todo = this.todoList.find((obj) => obj.id === idToFind);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    },
+  },
+  //getters
+});
